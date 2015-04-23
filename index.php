d5eb7b4b4cc5adc0c8367352becc2244974f2dc0
Martin-Adam <copperdog08@gmail.com>
2015-04-22 21:07:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <html>
 <head>
-<title>Hello World</title>
+<title>CS 313 Adam Martin</title>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
 
-<h1>Hello World</h1>
-<p>It's a trap!</p>
+<h1>Welcome to my Homepage</h1>
+<form>
 <a href="team0/testrun.html">Team Activity 0</a>
+<a href="helloWorld.html">Hello World</a>
+</form>
 </body>
 </html>
