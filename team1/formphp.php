30e51801e35c618e7f7c110f8ba30f4d678d850f
Martin-Adam <copperdog08@gmail.com>
2015-05-04 15:31:18 -0600

    Create formphp.php

---
+++ team1/formphp.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+Your name: <?php echo $_POST["name"]; ?>
+<br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>
+Major: <?php echo $_POST["major"]; ?>
+<br>
+Visted:
+<br>
+<?php
+foreach($_POST['places'] as $place) {
+     echo $place.'<br>';
+}
+?>
+<br>
+Comments:
+<br>
+<?php echo $_POST["comment"]; ?>
+</body>
+</html>
