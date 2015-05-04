39d35e7358fbbdf2b47e33313d71c1a8a084cf3d
Martin-Adam <copperdog08@gmail.com>
2015-05-04 15:36:39 -0600

    Update formphp.php

--- team1/formphp.php
+++ team1/formphp.php
@@ -5,6 +5,7 @@ Your name: <?php echo $_POST["name"]; ?>
 <br>
 Your email address is: <?php echo $_POST["email"]; ?>
 <br>
+<br>
 Major: <?php echo $_POST["major"]; ?>
 <br>
 Visted:
@@ -14,6 +15,7 @@ foreach($_POST['places'] as $place) {
      echo $place.'<br>';
 }
 ?>
+<br>
 <br>
 Comments:
 <br>
