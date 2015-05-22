e7605361431ddb5f53340cfa436d96c9ca096c8d
Martin-Adam <copperdog08@gmail.com>
2015-05-22 10:52:05 -0600

    Update phpDataBase.php

--- ls04/phpDataBase.php
+++ ls04/phpDataBase.php
@@ -4,12 +4,7 @@
 
 <?php
 
-    $llama = getenv('OPENSHIFT_APP_NAME');
-    echo "app name: $llama ::: "; 
-    
-    //$server = getenv('OPENSHIFT_MYSQL_DB_HOST');
     $server = getenv('OPENSHIFT_MYSQL_DB_HOST');
-    echo "Server: $server ";
     $dbname = 'store_db';
     $username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
     $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
@@ -21,7 +16,7 @@
         $link = new PDO($dsn, $username, $password, $options);
         $statement = $link->query('SELECT * FROM Users');
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        echo $results;
+        echo $results . "hi";
     } catch (Exception $ex) {
         echo "Fail".$ex;
     }
