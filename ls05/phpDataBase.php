fc024af43d68d105a336ed2a143f86929442dac3
Martin-Adam <copperdog08@gmail.com>
2015-05-30 21:39:53 -0600

    Update phpDataBase.php

--- ls05/phpDataBase.php
+++ ls05/phpDataBase.php
@@ -34,8 +34,9 @@
     try {
         $link = new PDO($dsn, $username, $password, $options);
         if(isset($_SESSION["userl"])){
-        	$sql = "SELECT a.* FROM Images a NATURAL LEFT JOIN Bought_items b WHERE b.images_id IS NULL;";
+        	$sql = "SELECT Images.* from Images inner join Bought_items inner join Users on Users.User_ID = Bought_items.User_ID where  Bought_items.images_id != Images.images_id AND Bought_items.User_ID = :name;";
         	$stmt = $link->prepare($sql);
+        	$stmt->bindParam(':name', $_SESSION["userl"]);
         	$stmt->execute();
         	$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
         	$stmt->closeCursor();
