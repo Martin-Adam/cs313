712f2f2744be0ea109459fa5d348fc50f0705eb6
Martin-Adam <copperdog08@gmail.com>
2015-05-30 22:32:09 -0600

    Update phpDataBase.php

--- ls05/phpDataBase.php
+++ ls05/phpDataBase.php
@@ -56,10 +56,29 @@
         	$stmt->closeCursor();
         	echo '<p align="right" style="color:white;"><a href="signin.php">Sign in/Create Account</a></p>';
         }
+        if(isset($_POST["submit"]) && isset($_SESSION["userl"])){
+        $pokemon = $_POST['pokemon']; 
+        if(!empty($pokemon)){
+		$n = count($pokemon);
+        	for($i=0; $i < $n; $i++)
+        	{
+        		//$sql = "SELECT * FROM Images;";
+	        //	$stmt = $link->prepare($sql);
+        	//	$stmt->execute();
+        	 //      	$stmt->closeCursor();
+
+        		echo($pokemon[$i] . " ");
+        	}
+        }	
+        
+        }
+        else if (isset($_POST["submit"])){
+        	echo 'Items will arrive close to never.';
+        }
         echo '<h1 style="color:white;">Buy your Plushies here!</h1>';
         
         $count = 0;
-        echo '<table align="center"><tr>';
+        echo '<form action="" method="post"><table align="center"><tr>';
         foreach($images as $i){
             if ($count == 3){
                 $count = 0;
@@ -70,9 +89,9 @@
             }
             echo '<td><img src="' . $i['image'] . '" alt="' . $i['name'] 
             . '" height="150" width="120"><br>' . $i['name'] 
-            . '<input type="checkbox" name="pokemon" value="'.$i['images_id'].'"></td>';
+            . '<input type="checkbox" name="pokemon[]" value="'.$i['images_id'].'"></td>';
         }
-        echo '</tr></table>';
+        echo '<td><input type="submit" name="submit" value="Submit">/td></tr></table></form>';
         
     } catch (Exception $ex) {
         echo "Fail".$ex;
