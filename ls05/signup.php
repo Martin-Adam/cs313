c8ef417a9a49d083e71f12b1f8cc025218ab4528
Martin-Adam <copperdog08@gmail.com>
2015-06-02 20:54:37 -0600

    Update signup.php

--- ls05/signup.php
+++ ls05/signup.php
@@ -17,38 +17,32 @@ Password:<input type="password" name="pass">
 </form>
 
 <?php
+if(isset($_POST['submit'])){
 	try{
-  $server = getenv('OPENSHIFT_MYSQL_DB_HOST');
-  $dbname = 'store_db';
-  $username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-  $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-  $dsn = 'mysql:host=' . $server . ';dbname=' . $dbname;
-  $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-  $db = new PDO($dsn, $username, $password, $options);
+	$server = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$dbname = 'store_db';
+	$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$dsn = 'mysql:host=' . $server . ';dbname=' . $dbname;
+	$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+	$db = new PDO($dsn, $username, $password, $options);
 
 	$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
-	$query = $db->query("SELECT Users WHERE User = '$_POST['user']'");
+	$query = $db->query('SELECT Users WHERE User = ' . $_POST['user']);
 
-  if($query)
-  {
-    echo 'it exists';
-  }
+  	if($query)
+	{
+    		echo 'it exists';
+  	}
 	else{
-	  echo 'it doesn't exist';
+		echo 'it doesnt exist';
 	}
-	
-	
-	//$stmt = $db->prepare('INSERT INTO `user`(`User_name`, `Password`) VALUES (:name,:pass)');
-	//$stmt->bindParam(':name', $_POST['user']);
-  //$stmt->bindParam(':pass', $pass);
-	//$stmt->execute();
-		
-	//header('Location: signin.php');
 	}
 	catch (PDOException $e){
 		echo $e;
 	}
+}
 ?>
 </body>
 </html>
