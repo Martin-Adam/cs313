8963e7a57a7b7e1d5ed2251a855fbb5e371b534d
Martin-Adam <copperdog08@gmail.com>
2015-06-04 22:40:22 -0600

    Update signup.php

--- ls05/signup.php
+++ ls05/signup.php
@@ -84,15 +84,12 @@ if(isset($_POST['submit']) && isset($_POST['user']) && isset($_POST['pass'])){
 	}
 	
 	if($message == ""){
-		echo 'hi2';
 		require 'password.php';
 		$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-		echo 'hi3';
 		$sql = "INSERT INTO `Users`(`User`, `Pass`) VALUES ('" . $_POST['user'] . "','" .$pass. "');";
         	$stmt = $link->prepare($sql);
          	$stmt->execute();
          	$stmt->closeCursor();
-        	echo 'hi4';
 		header('Location: signin.php');
 	}
 	else {
@@ -101,7 +98,7 @@ if(isset($_POST['submit']) && isset($_POST['user']) && isset($_POST['pass'])){
 	
 	}
 	catch (PDOException $e){
-		echo $e;
+		echo 'Sorry no connection, try again later';
 	}
 }
 ?>
